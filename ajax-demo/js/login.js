1cc6274a08bdac47b3f427468774549528384a15
huhuiyu <1069306849@qq.com>
2019-10-15 11:34:56 +0800

    登陆完成

--- ajax-demo/js/login.js
+++ ajax-demo/js/login.js
@@ -5,6 +5,7 @@
   let txtPwd = document.getElementById('txtPwd');
   let btnLogin = document.getElementById('btnLogin');
   let btnReset = document.getElementById('btnReset');
+  let divInfo = document.getElementById('divInfo');
 
   function resetInfo() {
     txtUser.value = '';
@@ -18,4 +19,32 @@
 
   resetInfo();
 
+  const server = 'http://huhuiyu.cn/ajax-demo';
+  const tokenKey = 'server.token';
+
+  btnLogin.addEventListener('click', function() {
+    //提交的用户信息格式要和服务器端一致
+    let userinfo = {
+      'tbUser.username': txtUser.value,
+      'tbUser.password': txtPwd.value
+    };
+
+    axios
+      .post(server + '/user/login', huhuiyu.JsonUtil.jsonToQs(userinfo), {
+        headers: { token: localStorage.getItem(tokenKey) }
+      })
+      .then(function(resp) {
+        let data = resp.data;
+        console.log(data);
+        //保存token
+        localStorage.setItem(tokenKey, data.token);
+        //通过success判断登陆是否成功
+        if (!data.success) {
+          //登陆失败会通过message提示失败的原因
+          divInfo.innerHTML = data.message;
+        } else {
+          location.href = 'main.html';
+        }
+      });
+  });
 })();
