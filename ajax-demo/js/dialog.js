b7d5c031295b05601ee7823bbacb84f8e393ce9b
huhuiyu <1069306849@qq.com>
2019-10-24 16:55:45 +0800

    公共对话框api

---
+++ ajax-demo/js/dialog.js
@@ -0,0 +1,48 @@
+(function() {
+  console.log('in dialog.js===>');
+  let dialog = {}; //对话框api
+  //确定对话框元素==================================
+  let alertDialog = $('#alertDialog'); //对话框
+  let btnAlertOk = $('#btnAlertOk'); //确定按钮
+  let alertOkFunction; //对话框关闭后的回调函数
+  //确定对话框的三个定制项
+  //title：对话框的标题，info：对话框显示的内容
+  //cb：对话框关闭的时候要执行的回调函数
+  dialog.showAlert = function(title, info, cb) {
+    $('#alertDialog .modal-title').html(title);
+    $('#alertDialog .modal-body').html(info);
+    //记录回调的函数，在对话框关闭后调用
+    alertOkFunction = cb ? cb : function() {};
+    alertDialog.modal('show');
+  };
+
+  btnAlertOk.click(function() {
+    alertDialog.modal('hide');
+  });
+
+  //关闭的时候调用关闭函数
+  alertDialog.on('hidden.bs.modal', function() {
+    alertOkFunction();
+  });
+
+  //等待对话框api
+  let waitDialog = $('#waitDialog');
+  let waitDialogCloseFunction;
+
+  dialog.showWait = function(info) {
+    $('#waitDialog .modal-body').html(info);
+    waitDialog.modal('show');
+  };
+
+  dialog.hideWait = function(cb) {
+    waitDialogCloseFunction = cb ? cb : function() {};
+    waitDialog.modal('hide');
+  };
+
+  waitDialog.on('hidden.bs.modal', function() {
+    waitDialogCloseFunction();
+  });
+
+  //放置对话框api到全局
+  window.dialog = dialog;
+})();
