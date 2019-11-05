3749201c98f6dd316eba3fcb325f79b1a7eaa52b
huhuiyu <1069306849@qq.com>
2019-11-05 08:02:44 +0800

    新版本登陆

---
+++ ajax-demo/mywebsite/js/login.js
@@ -0,0 +1,54 @@
+(function () {
+  console.log('in login.js====>');
+
+  let txtUsername = document.getElementById('txtUsername');
+  let txtPassword = document.getElementById('txtPassword');
+  let btnLogin = document.getElementById('btnLogin');
+  let btnReset = document.getElementById('btnReset');
+  let btnReg = document.getElementById('btnReg');
+  let divInfo = document.getElementById('divInfo');
+
+  /* 重置 */
+  function resetForm() {
+    txtUsername.value = '';
+    txtPassword.value = '';
+    divInfo.innerHTML = '';
+    txtUsername.focus();
+  }
+
+  btnReset.addEventListener('click', resetForm);
+  resetForm();
+
+  /* 登录 */
+  btnLogin.addEventListener('click', function () {
+    let user = {
+      username: txtUsername.value,
+      password: txtPassword.value
+    };
+    if (!user.username) {
+      divInfo.innerHTML = '用户名必须填写';
+      txtUsername.focus();
+      return;
+    }
+    if (!user.password) {
+      divInfo.innerHTML = '密码必须填写';
+      txtPassword.focus();
+      return;
+    }
+    divInfo.innerHTML = '';
+    dialog.showWait({ info: '登录中，请稍侯。。。' });
+    myapi.post('/user/login', { tbUser: user }
+      , function (data) {
+        dialog.hideWait();
+        if (data.success) {
+          location.href = 'main.html';
+        } else {
+          divInfo.innerHTML = data.message;
+          txtUsername.focus();
+        }
+      });
+
+
+  });
+
+})();
